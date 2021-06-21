b8d2f0263752cf6c46725e9abf836b02d0fe4ce3
MD Pear E Habib <habib.austcse31@gmail.com>
2021-06-21 17:06:49 +0600

    install uuid

--- src/components/CrudOperation.vue
+++ src/components/CrudOperation.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
         <h1>crud operation</h1>
+        {{getterBooks}}
+        
+
+
     </div>
 </template>
 
 <script>
+import {mapActions,mapGetters} from 'vuex'
+// import { v4 as uuidv4 } from 'uuid';
 export default {
 name:'CrudOperation',
+computed:{
+    ...mapGetters(['getterBooks']),
+},
+methods:{
+     ...mapActions(['']),
+
+},
+data(){
+    return{
+        obj:{
+            id:'',
+            title:''
+        }
+    }
+}
 
 }
 </script>
--- src/store/modules/crud.js
+++ src/store/modules/crud.js
@@ -1,10 +1,17 @@
 // import axios from 'axios'
  
 const state={
-    
+    books:[
+        {
+            id:1,
+            title:'this is book 1'
+        }
+    ]
 };
 const getters={
-   
+   getterBooks(state){
+       return state.books;
+   }
 };
 const actions={
     
